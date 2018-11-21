eb2adae75f5cd57a3a4069368be4299cfdd971bc
Akshay Patidar <akshaypatidar1999@gmail.com>
2018-11-21 14:37:22 +0530

    Sample Images Added

--- get_patches.m
+++ get_patches.m
@@ -2,7 +2,7 @@ function [ patches ] = get_patches( input_image, patch_size )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
     patches = im2col(input_image,[patch_size,patch_size],'sliding');
-    patches = patches(:,1:2:end,:);
+    patches = patches(:,1:1:end,:);
     fprintf('Total Number of Patches = %d \n',size(patches,2));
     
 end
